307e03375202bc49db3a6a41ba03935f840938e4
Carvier <60159324+carvierdotdev@users.noreply.github.com>
2024-01-28 05:18:52 -0500

    [Chapter 9] Fix: Move loading.tsx and page.tsx to /(overview) folder
    This commit moves the loading.tsx and page.tsx files from the dashboard folder to a new folder called /(overview) inside the dashboard folder. This fixes the loading skeleton bug that applied to the invoices and customers pages as well. This change follows the fixing the loading skeleton bug with route groups section of Chapter 9 in the Next.js Learn course, which teaches how to use route groups to control the loading state of different pages.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,7 +6,7 @@ import {
 import { Card } from '@/app/ui/dashboard/cards';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import { lusitana } from '../ui/fonts/fonts';
+import { lusitana } from '../../ui/fonts/fonts';
 
 export default async function Page() {
   const revenue = await fetchRevenue();
