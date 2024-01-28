5dda2795d83ef04c97f126630cff2f0e0db7fc0a
Carvier <60159324+carvierdotdev@users.noreply.github.com>
2024-01-28 05:57:49 -0500

    [Chapter 9] Feat: Group Card components with a wrapper component
    This commit modifies the page.tsx and cards.tsx files to group the Card components with a wrapper component. This wraps the Card components in a Suspense boundary and creates a staggered effect for loading the cards. This change follows the grouping components section of Chapter 9 in the Next.js Learn course, which teaches how to use streaming to improve the performance and user experience of your website.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,8 +1,8 @@
-import { fetchCardData } from '@/app/lib/data';
-import { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper from '@/app/ui/dashboard/cards';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import {
+  CardsSkeleton,
   LatestInvoicesSkeleton,
   RevenueChartSkeleton,
 } from '@/app/ui/skeletons';
@@ -12,12 +12,12 @@ import { lusitana } from '../../ui/fonts/fonts';
 export default async function Page() {
   // const revenue = await fetchRevenue(); // Delete of line to use Suspense for more granular control over the component who has a delay..
   // const latestInvoices = await fetchLatestInvoices(); // Delete of line to use Suspense for more granular control over the component who has a delay..
-  const {
-    numberOfCustomers,
-    numberOfInvoices,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
+  // const {
+  //   numberOfCustomers,
+  //   numberOfInvoices,
+  //   totalPaidInvoices,
+  //   totalPendingInvoices,
+  // } = await fetchCardData();
 
   return (
     <main>
@@ -25,14 +25,9 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        />
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
