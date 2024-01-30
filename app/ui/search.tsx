fc71c931f0d1eb98dab0ce19b3feae6549449a29
Carvier <60159324+carvierdotdev@users.noreply.github.com>
2024-01-30 11:07:32 -0500

    [Chapter 11] Feat: Update URL with Search Params
    This commit updates the URL with the search parameters in the 'page.tsx' file. This change follows the 'Adding Search and Pagination' section of Chapter 11 in the Next.js Learn course, which teaches how to update the URL with the search parameters based on the user's input. The changes include using the 'useSearchParams', 'usePathname', and 'useRouter' hooks from Next.js to capture the search parameters and update the URL accordingly.

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,10 +1,22 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  function handleChange(term: string) {
-    console.log(term);
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  function handleSearch(term: string) {
+    const params = new URLSearchParams(searchParams);
+
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
   }
 
   return (
@@ -16,7 +28,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => {
-          handleChange(e.target.value);
+          handleSearch(e.target.value);
         }}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
